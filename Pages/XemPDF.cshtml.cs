fc9e0cf3b26defb48d018456a7bd41274d31427c
muahe2004 <minh.ly@blueoc.tech>
2026-04-13 21:15:09 +0700

    initial commit

---
+++ Pages/XemPDF.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class XemPDFModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
